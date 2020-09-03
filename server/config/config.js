77a1ad762e264e1df182156108d52d1e5353f0a8
Jalexss <jhalx2198@gmail.com>
2020-09-02 19:32:44 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -5,3 +5,24 @@
 =============================================*/
 
 port =  process.env.PORT || 3000;
+
+/*=============================================
+    Entorno
+=============================================*/
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+/*=============================================
+    Base de datos
+=============================================*/
+
+let urlDB;
+
+if( process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else {
+    urlDB = 'mongodb+srv://crawsk08:5Ifm7G0L5fRD0H3M@cluster0.jqchj.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
